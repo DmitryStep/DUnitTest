f1e80c32fe66df295ac95f25ea83fb8f49f59de2
Dmitry Stepanov <Dstepanov@ils-glonass.ru>
2017-10-20 16:25:13 +0500

    ReadTests ready

--- TestScript.php
+++ TestScript.php
@@ -1,13 +1,36 @@
 ﻿<?php
 
-// Read file
-$testfile = fopen("tests.txt", "r");
-
-while (!feof($testfile)){
-  echo fgets($testfile)."<br>";
+function readtestfile($input_file){
+  $tests_array = "";
+  $testfile = fopen($input_file, "r");
+  $Index = 0;
+  while (!feof($testfile)){
+    $tests_array[$Index] = fgets($testfile);
+    $Index++;
+  }
+  fclose($testfile);
+  return $tests_array;
 }
 
-fclose($testfile);
+function FormRequest($URL, $Request, $params){
+}
 
+function SendRequestAndGetResponse($request){
+}
+
+function ParseJSONResponce(){
+}
+
+function AssertResults(){
+}
+
+function OutputToXMLLog($output_file){
+}
+
+$tests = readtestfile("tests.txt");
+
+foreach ($tests as $test) {
+  echo $test."<br>";
+}
 
 ?>
