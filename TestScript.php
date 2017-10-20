936dde52c375f2710b86bcd0cb5b89d3fd3f41ad
Dmitry Stepanov <Dstepanov@ils-glonass.ru>
2017-10-20 18:13:05 +0500

    Sceleton of script

--- TestScript.php
+++ TestScript.php
@@ -1,5 +1,8 @@
 ﻿<?php
 
+// functions
+
+// Read from file
 function readtestfile($input_file){
   $tests_array = "";
   $testfile = fopen($input_file, "r");
@@ -12,20 +15,31 @@ function readtestfile($input_file){
   return $tests_array;
 }
 
+// Request form
 function FormRequest($URL, $Request, $params){
 }
 
-function SendRequestAndGetResponse($request){
-}
-
+// Parse JSON
 function ParseJSONResponce(){
 }
 
+// Send request and Get response in JSON
+function SendRequestAndGetResponse($request){
+}
+
+// Assert test results
 function AssertResults(){
 }
 
+// Output to log in NUnit-format
 function OutputToXMLLog($output_file){
 }
+
+// ---------------------------- End of functions -------------------------------
+
+// Main Program
+
+$URL = "http://test.ils-glonass.ru";
 
 $tests = readtestfile("tests.txt");
 
